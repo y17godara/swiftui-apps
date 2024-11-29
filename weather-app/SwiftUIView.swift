bafab521dbf3a5d1004a2d4a50955a7fac30e0e2
Yash Godara <yash17godara@gmail.com>
2024-11-30 00:35:35 +0530

    fix

---
+++ weather-app/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  weather-app
+//
+//  Created by Yash Godara on 30/11/24.
+//
+
+import SwiftUI
+
+struct WelcomeUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeUIView()
+}
